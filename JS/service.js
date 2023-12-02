56b98f995bebcde73b458450867e9267270c1587
rxfe.a <104333985+rxfe-a@users.noreply.github.com>
2023-12-01 17:36:21 -0800

    Auto Detect

---
+++ JS/service.js
@@ -0,0 +1,25 @@
+function checkSiteStatus(siteUrl, onlineRedirect, offlineRedirect) {
+    fetch(siteUrl, { method: 'HEAD' })
+      .then(response => {
+        if (response.ok) {
+          // Site is online, redirect to the provided URL
+          window.location.href = onlineRedirect;
+        } else {
+          // Site is offline, redirect to the other page
+          window.location.href = offlineRedirect;
+        }
+      })
+      .catch(error => {
+        // Error occurred, assume site is offline and redirect
+        window.location.href = offlineRedirect;
+      });
+  }
+  
+  // Usage
+  function testConnection() {
+    const siteUrl = 'https://2mdfmnqv-8080.usw3.devtunnels.ms/';
+  const onlineRedirect = siteUrl;
+  const offlineRedirect = 'https://seismetic.pages.dev/';
+  
+  checkSiteStatus(siteUrl, onlineRedirect, offlineRedirect);
+  }
