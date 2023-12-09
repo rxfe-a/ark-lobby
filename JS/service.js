737a4e1f0deae7db61a0741699bee2bb39b847c8
rxfe.a <104333985+rxfe-a@users.noreply.github.com>
2023-12-08 22:14:01 -0800

    acoustic

--- JS/service.js
+++ JS/service.js
@@ -1,9 +1,9 @@
 function go() {
-  window.location.href='https://2mdfmnqv-8080.usw3.devtunnels.ms/'
+  window.location.href='https://2mdfmnqv-8080.usw3.devtunnels.ms/' + localStorage.getItem('username')
 const butt = document.getElementById('TitansGO')
 butt.style.backgroundColor = '#333333';
 butt.textContent = 'FINDING SERVER';
 setTimeout(() => {
-  window.location.href='https://sm145.vercel.app/';
+  window.location.href='https://sm1456.vercel.app/#' + localStorage.getItem('username');
 }, 7000);
 }
